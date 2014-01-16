d711cc366159753b1236ade6182635878c4f1b92
Gordon Sim <gsim@apache.org>
2014-01-16 13:23:31 +0000

    Initial fixes for pep8 violations in proton.py

--- oslo/messaging/_drivers/amqp10/proton.py
+++ oslo/messaging/_drivers/amqp10/proton.py
@@ -13,27 +13,23 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import functools
-import itertools
+import errno
 import logging
-import time
 import os
+import re
+import select
+import socket
+import threading
+import time
+import uuid
 
-import eventlet
-import greenlet
 from oslo.config import cfg
 
 from six import moves
 
-from oslo.messaging._drivers import base
 from oslo.messaging._drivers.amqp10 import proton_wrapper
-
-#from oslo.messaging._drivers import amqp as rpc_amqp
-#from oslo.messaging._drivers import amqpdriver
-#from oslo.messaging._drivers import common as rpc_common
-#from oslo.messaging.openstack.common import excutils
-#from oslo.messaging.openstack.common import importutils
-#from oslo.messaging.openstack.common import jsonutils
+from oslo.messaging._drivers import base
+from oslo.messaging.openstack.common import importutils
 
 # FIXME(markmc): remove this
 _ = lambda s: s
@@ -45,13 +41,13 @@ LOG = logging.getLogger(__name__)
 proton_opts = [
     cfg.StrOpt('server_request_prefix',
                default='',
-               help="address prefix used when sending to a specific server")
+               help="address prefix used when sending to a specific server"),
     cfg.StrOpt('broadcast_prefix',
                default='',
-               help="address prefix used when broadcasting to all servers")
+               help="address prefix used when broadcasting to all servers"),
     cfg.StrOpt('group_request_prefix',
                default='',
-               help="address prefix used when load-balancing across a group of servers")
+               help="address prefix when sending to any server in group"),
 
     cfg.StrOpt('gateway_hostname',
                default='localhost',
@@ -84,7 +80,7 @@ proton_opts = [
     # for now:
     cfg.BoolOpt('amqp10_point_to_point',
                 default=False,
-                help='Enable point-to-point messaging.')
+                help='Enable point-to-point messaging.'),
 
     cfg.BoolOpt('proton_trace',
                 default=False,
@@ -95,7 +91,7 @@ JSON_CONTENT_TYPE = 'application/json; charset=utf8'
 
 
 class _SocketConnection(proton_wrapper.ConnectionEventHandler):
-    """Associates a proton Connection with a python network socket, 
+    """Associates a proton Connection with a python network socket,
     and handles all connection-related events.
     """
 
@@ -117,55 +113,53 @@ class _SocketConnection(proton_wrapper.ConnectionEventHandler):
         return self.socket.fileno()
 
     def process_input(self):
-        """Called when socket is read-ready"""
+        """Called when socket is read-ready."""
         try:
-            rc = fusion.read_socket_input(self.connection,
-                                          self.socket)
-        except:
-            rc = Connection.EOS
+            rc = proton_wrapper.sockets.read_socket_input(self.connection,
+                                                          self.socket)
+        except Exception:
+            rc = proton_wrapper.Connection.EOS
         if rc > 0:
             self.connection.process(time.time())
         return rc
 
     def send_output(self):
-        """Called when socket is write-ready"""
+        """Called when socket is write-ready."""
         try:
-            rc = fusion.write_socket_output(self.connection,
-                                            self.socket)
-        except:
-            rc = Connection.EOS
+            rc = proton_wrapper.sockets.write_socket_output(self.connection,
+                                                            self.socket)
+        except Exception:
+            rc = proton_wrapper.Connection.EOS
         if rc > 0:
             self.connection.process(time.time())
         return rc
 
     # ConnectionEventHandler callbacks
-    # Note well: all of these callbacks are invoked while the proton_lock is held
+    # Note well: these callbacks are all invoked while the proton_lock is held
 
     def connection_active(self, connection):
-        print "APP: CONN ACTIVE"
+        LOG.info("APP: CONN ACTIVE")
 
     def connection_closed(self, connection, reason):
-        print "APP: CONN CLOSED"
+        LOG.info("APP: CONN CLOSED")
 
     def sender_requested(self, connection, link_handle,
                          requested_source, properties):
         ## @todo: support needed for peer-2-peer
-        print "APP: SENDER LINK REQUESTED"
+        LOG.info("APP: SENDER LINK REQUESTED")
 
     def receiver_requested(self, connection, link_handle,
                            requested_target, properties):
         ## @todo: support needed for peer-2-peer
-        print "APP: RECEIVER LINK REQUESTED"
+        LOG.info("APP: RECEIVER LINK REQUESTED")
 
     # SASL callbacks:
 
     def sasl_step(self, connection, pn_sasl):
-        print "SASL STEP"
+        LOG.info("SASL STEP")
 
     def sasl_done(self, connection, result):
-        print "APP: SASL DONE"
-        print result
-
+        LOG.info("APP: SASL DONE: %s" % result)
 
 
 class ProtonDriver(base.BaseDriver, threading.Thread):
@@ -198,20 +192,17 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
         else:
             self._container = proton_wrapper.Container(uuid.uuid4().hex)
 
-
-
         # run the Proton Engine event/timer thread:
-
-        self._wakeup_pipe = os.Pipe() # (r,w) - to force return from select()
+        self._wakeup_pipe = os.Pipe()  # (r,w) - to force return from select()
         self._proton_lock = threading.Lock()
 
         self.name = "Thread for Proton container: %s" % self._container.name
         self._shutdown = False
-        self.daemon = False # KAG: or True ???
+        self.daemon = False  # KAG: or True ???
         self.start()
 
     def wakeup(self):
-        """Force the Proton thread to wake up"""
+        """Force the Proton thread to wake up."""
         os.write(self._wakeup_pipe[1], "!")
 
     def destroy(self):
@@ -223,13 +214,13 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
         self.join()
 
     def get_connection(self, url, properties={}):
-        """Get a _SocketConnection to a peer represented by url"""
+        """Get a _SocketConnection to a peer represented by url."""
         # @todo KAG: for now, assume amqp://<hostname>[:port]
         regex = re.compile(r"^amqp://([a-zA-Z0-9.]+)(:([\d]+))?$")
         x = regex.match(url)
         if not x:
-            error = "Bad URL syntax: %s" % url
-            LOG.warn(_(error))
+            error = _("Bad URL syntax: %s") % url
+            LOG.warn(error)
             raise Exception(error)
 
         # return pre-existing
@@ -243,19 +234,20 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
         matches = x.groups()
         host = matches[0]
         port = int(matches[2]) if matches[2] else None
-        addr = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+        addr = socket.getaddrinfo(host, port,
+                                  socket.AF_INET, socket.SOCK_STREAM)
         if not addr:
-            error = "Could not translate address '%s'" % url
-            LOG.warn(_(error))
+            error = _("Could not translate address '%s'") % url
+            LOG.warn(error)
             raise Exception(error)
         my_socket = socket.socket(addr[0][0], addr[0][1], addr[0][2])
-        my_socket.setblocking(0) # 0=non-blocking
+        my_socket.setblocking(0)  # 0=non-blocking
         try:
             my_socket.connect(addr[0][4])
-        except socket.error, e:
+        except socket.error as e:
             if e[0] != errno.EINPROGRESS:
-                error = "Socket connect failure '%s'" % str(e)
-                LOG.warn(_(error))
+                error = _("Socket connect failure '%s'") % str(e)
+                LOG.warn(error)
                 raise Exception(error)
 
         # create a new connection - this will be stored in the container,
@@ -271,19 +263,17 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
                 pn_sasl.client()
         return sc
 
-
     def run(self):
-        """Proton event/timer thread"""
-
-        LOG.debug(_("Starting Proton thread container=%s") % self._container.name))
+        """Run the proton event/timer loop."""
+        LOG.debug(_("Starting Proton thread, container=%s"),
+                  self._container.name)
 
         while not self._shutdown:
-
             readfd = [self._wakeup_pipe[0]]
             writefd = []
 
             with self._proton_lock:
-                readers,writers,timers = container.need_processing()
+                readers, writers, timers = self._container.need_processing()
 
             # map fusion Connections back to my SocketConnections
             for c in readers:
@@ -297,20 +287,25 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
 
             timeout = None
             if timers:
-                deadline = timers[0].next_tick # 0 == next expiring timer
+                deadline = timers[0].next_tick  # 0 == next expiring timer
                 now = time.time()
                 timeout = 0 if deadline <= now else deadline - now
 
-            LOG.debug(_("proton thread select() call (timeout=%s)" % str(timeout)))
-            readable,writable,ignore = select.select(readfd,writefd,[],timeout)
+            LOG.debug(_("proton thread select() call (timeout=%s)"),
+                      str(timeout))
+            readable, writable, ignore = select.select(readfd,
+                                                       writefd,
+                                                       [],
+                                                       timeout)
             LOG.debug(_("select() returned"))
 
             while os.read(self._wakeup_pipe[0], 512):
                 pass
 
             for r in readable:
-                if r is self._wakeup_pipe[0]: continue
-                assert isinstance(r, SocketConnection)
+                if r is self._wakeup_pipe[0]:
+                    continue
+                assert isinstance(r, _SocketConnection)
 
                 # @todo: KAG update to use utility method
                 with self._proton_lock:
@@ -319,25 +314,26 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
                         try:
                             sock_data = r.socket.recv(count)
                             if sock_data:
-                                r.connection.process_input( sock_data )
+                                r.connection.process_input(sock_data)
                             else:
                                 # closed?
                                 r.connection.close_input()
-                        except socket.timeout, e:
+                        except socket.timeout as e:
                             # I don't expect this
-                            LOG.warn(_("Unexpected socket timeout %s" % str(e)))
-                        except socket.error, e:
+                            LOG.warn(_("Unexpected socket timeout %s"), str(e))
+                        except socket.error as e:
                             err = e.args[0]
                             # ignore non-fatal errors
                             if (err != errno.EAGAIN and
-                                err != errno.EWOULDBLOCK and
-                                err != errno.EINTR):
+                                    err != errno.EWOULDBLOCK and
+                                    err != errno.EINTR):
                                 # otherwise, unrecoverable:
                                 r.connection.close_input()
-                                LOG.warn(_("Unexpected socket error %s" % str(e)))
-                        except Exception, e:  # beats me...
+                                LOG.warn(_("Unexpected socket error %s"),
+                                         str(e))
+                        except Exception as e:  # beats me...
                             r.connection.close_input()
-                            LOG.warn(_("Unknown socket error" % str(e)))
+                            LOG.warn(_("Unknown socket error"), str(e))
 
                         r.connection.process(time.time())
 
@@ -349,7 +345,7 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
 
             for w in writable:
                 # @todo: KAG update to use utility method
-                assert isinstance(w, SocketConnection)
+                assert isinstance(w, _SocketConnection)
                 with self._proton_lock:
                     data = w.connection.output_data()
                     if data:
@@ -360,33 +356,34 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
                             else:
                                 # else socket closed
                                 w.connection.close_output()
-                        except socket.timeout, e:
+                        except socket.timeout as e:
                             # I don't expect this
-                            LOG.warn(_("Unexpected socket timeout %s" % str(e)))
-                        except socket.error, e:
+                            LOG.warn(_("Unexpected socket timeout %s"), str(e))
+                        except socket.error as e:
                             err = e.args[0]
                             # ignore non-fatal errors
                             if (err != errno.EAGAIN and
-                                err != errno.EWOULDBLOCK and
-                                err != errno.EINTR):
+                                    err != errno.EWOULDBLOCK and
+                                    err != errno.EINTR):
                                 # otherwise, unrecoverable
                                 w.connection.close_output()
-                                LOG.warn(_("Unexpected socket error %s" % str(e)))
-                        except Exception, e:  # beats me...
+                                LOG.warn(_("Unexpected socket error %s"),
+                                         str(e))
+                        except Exception as e:  # beats me...
                             w.connection.close_output()
-                            LOG.warn(_("Unknown socket error %s" % str(e)))
+                            LOG.warn(_("Unknown socket error %s"),
+                                     str(e))
                         w.connection.process(time.time())
 
-        LOG.debug(_("Stopping Proton thread, container=%s") % self._container.name))
+        LOG.debug(_("Stopping Proton thread, container=%s"),
+                  self._container.name)
 
         # abort any requests. Setting the lists to None here prevents further
         # requests from being created
 
         print("TODO: clean shutdown")
 
-    #
     # BaseDriver Interface:
-    #
     def send(self, target, ctxt, message,
              wait_for_reply=None, timeout=None, envelope=False):
         """Send a message to the given target."""
@@ -404,5 +401,3 @@ class ProtonDriver(base.BaseDriver, threading.Thread):
         """Release all resources."""
         assert False, "NOT YET IMPLEMENTED"
         self._daemon.destroy()
-
-
