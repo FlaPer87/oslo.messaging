88a6dd058b363f81150be2db5142a6fa9a3fa74c
Gordon Sim <gsim@apache.org>
2014-03-04 15:45:28 +0000

    New branch reflecting the review I attached to the blueprint

---
+++ oslo/messaging/_drivers/protocols/amqp/driver.py
@@ -0,0 +1,299 @@
+#    Copyright 2014 OpenStack Foundation
+#    Copyright 2014, Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import abc
+import logging
+import threading
+
+from six import moves
+
+from oslo.config import cfg
+from oslo.messaging._drivers import base
+from oslo.messaging._drivers import common
+from oslo.messaging._drivers.protocols.amqp import controller
+from oslo.messaging.openstack.common import importutils
+from oslo.messaging.openstack.common import jsonutils
+
+proton = importutils.try_import("proton")
+
+LOG = logging.getLogger(__name__)
+
+proton_opts = [
+    cfg.StrOpt('server_request_prefix',
+               default='',
+               help="address prefix used when sending to a specific server"),
+    cfg.StrOpt('broadcast_prefix',
+               default='',
+               help="address prefix used when broadcasting to all servers"),
+    cfg.StrOpt('group_request_prefix',
+               default='',
+               help="address prefix when sending to any server in group"),
+
+    cfg.StrOpt('amqp_container_name',
+               default='',
+               help='Name for the AMQP container'),
+
+    cfg.IntOpt('amqp_idle_timeout',
+               default=0,  # disabled
+               help='Timeout for inactive connections (in seconds)'),
+
+    cfg.BoolOpt('amqp_trace',
+                default=False,
+                help='Debug: dump AMQP frames to stdout'),
+
+    cfg.StrOpt('proton_ca_file',
+               default='',
+               help="CA certificate PEM file for verifing server certificate"),
+
+    cfg.StrOpt('proton_cert_file',
+               default='',
+               help='Identifying certificate to present to clients'),
+
+    cfg.StrOpt('proton_key_file',
+               default='',
+               help='Private key used to sign cert_file certificate'),
+
+    cfg.StrOpt('proton_key_password',
+               default=None,
+               help='Password for decrypting key_file (if encrypted)'),
+
+    cfg.BoolOpt('allow_insecure_clients',
+                default=False,
+                help='Accept clients using either SSL or plain TCP')
+]
+
+
+class Task(object):
+    @abc.abstractmethod
+    def execute(self, manager):
+        """Perform operation on the protocol manager (will be called
+        on a thread of its choosing).
+        """
+
+
+class SendTask(Task):
+    def __init__(self, target, request, reply_expected):
+        self._target = target
+        self._request = request
+        if reply_expected:
+            self._reply_queue = moves.queue.Queue()
+        else:
+            self._reply_queue = None
+
+    def execute(self, manager):
+        manager.request(self._target, self._request, self._reply_queue)
+
+    def get_reply(self, timeout):
+        if not self._reply_queue:
+            return None
+        return self._reply_queue.get(timeout)
+
+
+class ListenTask(Task):
+    def __init__(self, target, incoming, notifications=False):
+        self._target = target
+        self._incoming = incoming
+        self._notifications = notifications
+
+    def execute(self, manager):
+        if self._notifications:
+            manager.subscribe_notifications(self._target, self._incoming)
+        else:
+            manager.subscribe(self._target, self._incoming)
+
+
+class ReplyTask(Task):
+    def __init__(self, address, response, log_failure):
+        self._address = address
+        self._response = response
+        self._log_failure = log_failure
+
+    def execute(self, manager):
+        manager.response(self._address, self._response)
+
+
+def marshal_response(reply=None, failure=None):
+    #TODO(grs): do replies have a context?
+    msg = proton.Message()
+    if failure:
+        failure = common.serialize_remote_exception(failure)
+        data = {"failure": failure}
+    else:
+        data = {"response": reply}
+    msg.body = jsonutils.dumps(data)
+    return msg
+
+
+def unmarshal_response(message, allowed):
+    data = jsonutils.loads(message.body)
+    if "response" in data:
+        return data["response"]
+    elif "failure" in data:
+        #TODO(grs)
+        failure = data["failure"]
+        return common.deserialize_remote_exception(failure, allowed)
+    else:
+        #TODO(grs)
+        #???
+        return data
+
+
+def marshal_request(request, context, envelope):
+    msg = proton.Message()
+    if envelope:
+        request = common.serialize_msg(request)
+    data = {
+        "request": request,
+        "context": context
+    }
+    msg.body = jsonutils.dumps(data)
+    return msg
+
+
+def unmarshal_request(message):
+    data = jsonutils.loads(message.body)
+    if "request" in data:
+        request = data["request"]
+    if "context" in data:
+        context = data["context"]
+    return (request, context)
+
+
+class ProtonIncomingMessage(base.IncomingMessage):
+    def __init__(self, listener, ctxt, request, task_queue, message):
+        base.IncomingMessage.__init__(self, listener, ctxt, request)
+        self._task_queue = task_queue
+        self._reply_to = message.reply_to
+        self._correlation_id = message.id
+
+    def reply(self, reply=None, failure=None, log_failure=True):
+        if self._reply_to:
+            response = marshal_response(reply=reply, failure=failure)
+            if self._correlation_id:
+                response.correlation_id = self._correlation_id
+            LOG.debug("Replying to %s" % self._correlation_id)
+            task = ReplyTask(self._reply_to, response, log_failure)
+            self._task_queue.put(task)
+        else:
+            LOG.warn("Ignoring reply as no reply address available")
+
+
+class ProtonListener(base.Listener):
+    def __init__(self, driver, tasks):
+        base.Listener.__init__(self, driver)
+        self._tasks = tasks
+        self._incoming = moves.queue.Queue()
+
+    def poll(self):
+        message = self._incoming.get()
+        request, ctxt = unmarshal_request(message)
+        LOG.debug("Returning incoming message")
+        return ProtonIncomingMessage(self, ctxt, request, self._tasks, message)
+
+    def incoming(self):
+        return self._incoming
+
+
+class ProtonDriver(base.BaseDriver):
+
+    def __init__(self, conf, url,
+                 default_exchange=None, allowed_remote_exmods=[]):
+        base.BaseDriver.__init__(self, conf, url, default_exchange,
+                                 allowed_remote_exmods)
+        conf.register_opts(proton_opts)
+        # TODO(grs): handle authentication etc
+        hosts = [(h.hostname, h.port or 5672) for h in url.hosts]
+        cname = conf.amqp_container_name if conf.amqp_container_name else None
+        self._mgr = controller.Controller(hosts, cname)
+        if conf.server_request_prefix:
+            self._mgr.server_request_prefix = conf.server_request_prefix
+        if conf.broadcast_prefix:
+            self._mgr.broadcast_prefix = conf.broadcast_prefix
+        if conf.group_request_prefix:
+            self._mgr.group_request_prefix = conf.group_request_prefix
+        self._mgr.default_exchange = default_exchange
+        self._mgr.idle_timeout = conf.amqp_idle_timeout
+        self._mgr.trace_protocol = conf.amqp_trace
+
+        # SSL configuration
+        self._mgr.ssl_ca_file = conf.proton_ca_file
+        self._mgr.ssl_cert_file = conf.proton_cert_file
+        self._mgr.ssl_key_file = conf.proton_key_file
+        self._mgr.ssl_key_password = conf.proton_key_password
+        self._mgr.ssl_allow_insecure = conf.allow_insecure_clients
+
+        self._connected = False
+        self._lock = threading.Lock()
+
+    def send(self, target, ctxt, message,
+             wait_for_reply=None, timeout=None, envelope=False):
+        """Send a message to the given target."""
+        self._ensure_connected()
+        request = marshal_request(message, ctxt, envelope)
+        if timeout:
+            request.ttl = timeout
+        task = SendTask(target, request, wait_for_reply)
+        self._mgr.tasks().put(task)
+        if wait_for_reply:
+            LOG.debug("Send to %s, response expected" % target)
+            reply = task.get_reply(timeout)
+            result = unmarshal_response(reply, self._allowed_remote_exmods)
+            LOG.debug("Send to %s returning" % target)
+            return result
+        else:
+            LOG.debug("Send to %s, no response expected" % target)
+            return None
+
+    def send_notification(self, target, ctxt, message, version):
+        """Send a notification message to the given target."""
+        self._ensure_connected()
+        return self.send(target, ctxt, message, envelope=(version == 2.0))
+
+    def listen(self, target):
+        """Construct a Listener for the given target."""
+        LOG.debug("Listen to %s" % target)
+        self._ensure_connected()
+        listener = ProtonListener(self, self._mgr.tasks())
+        self._mgr.tasks().put(ListenTask(target, listener._incoming))
+        return listener
+
+    def listen_for_notifications(self, targets_and_priorities):
+        LOG.debug("Listen for notifications %s" % targets_and_priorities)
+        self._ensure_connected()
+        listener = ProtonListener(self, self._mgr.tasks())
+        for target, priority in targets_and_priorities:
+            target.topic = '%s.%s' % (target.topic, priority)
+            self._mgr.tasks().put(ListenTask(target, listener._incoming, True))
+        return listener
+
+    def _need_connect(self):
+        with self._lock:
+            if not self._connected:
+                self._connected = True
+                return True
+            else:
+                return False
+
+    def _ensure_connected(self):
+        # Cause manager to connect when first used. It is safe to push
+        # tasks to it whether connected or not, but those tasks won't
+        # be processed until connection completes.
+        if self._need_connect():
+            self._mgr.connect()
+
+    def cleanup(self):
+        """Release all resources."""
+        LOG.debug("Cleaning up ProtonDriver")
+        self._mgr.destroy()
---
+++ oslo/messaging/_drivers/protocols/amqp/eventloop.py
@@ -0,0 +1,266 @@
+#    Copyright 2014 OpenStack Foundation
+#    Copyright 2014, Red Hat, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import errno
+import heapq
+import logging
+import os
+import select
+import socket
+import threading
+import time
+import uuid
+
+from six import moves
+
+from oslo.messaging._drivers.protocols.amqp import engine
+
+LOG = logging.getLogger(__name__)
+
+
+class _SocketConnection():
+    """Associates a proton Connection with a python network socket,
+    and handles all connection-related events.
+    """
+
+    def __init__(self, name, container, properties, handler):
+        self.name = name
+        self._properties = properties
+        self._handler = handler
+        self._container = container
+        c = container.create_connection(name, handler, properties)
+        c.user_context = self
+        self.connection = c
+
+    def fileno(self):
+        """Allows use of a _SocketConnection in a select() call.
+        """
+        return self.socket.fileno()
+
+    def read(self):
+        """Called when socket is read-ready."""
+        while True:
+            try:
+                rc = engine.do_input(self.connection, self.socket)
+                if rc > 0:
+                    self.connection.process(time.time())
+                return rc
+            except socket.error as e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EINTR:
+                    continue
+                elif err == errno.EWOULDBLOCK:
+                    return 0
+                else:
+                    self._handler.connection_failed(self.connection, e)
+                    return engine.Connection.EOS
+
+    def write(self):
+        """Called when socket is write-ready."""
+        while True:
+            try:
+                rc = engine.do_output(self.connection, self.socket)
+                if rc > 0:
+                    self.connection.process(time.time())
+                return rc
+            except socket.error as e:
+                err = e.args[0]
+                if err == errno.EAGAIN or err == errno.EINTR:
+                    continue
+                elif err == errno.EWOULDBLOCK:
+                    return 0
+                else:
+                    self._handler.connection_failed(self.connection, e)
+                    return engine.Connection.EOS
+
+    def connect(self, hostname, port, sasl_mechanisms="ANONYMOUS"):
+        addr = socket.getaddrinfo(hostname, port,
+                                  socket.AF_INET, socket.SOCK_STREAM)
+        if not addr:
+            key = "%s:%i" % (hostname, port)
+            error = _("Could not translate address '%s'") % key
+            LOG.warn(error)
+            self.handler.connection_failed(self.connection, error)
+        my_socket = socket.socket(addr[0][0], addr[0][1], addr[0][2])
+        my_socket.setblocking(0)  # 0=non-blocking
+        try:
+            my_socket.connect(addr[0][4])
+        except socket.error as e:
+            if e[0] != errno.EINPROGRESS:
+                error = _("Socket connect failure '%s'") % str(e)
+                LOG.warn(error)
+                self.handler.connection_failed(self.connection, str(e))
+        self.socket = my_socket
+
+        if sasl_mechanisms:
+            pn_sasl = self.connection.sasl
+            pn_sasl.mechanisms(sasl_mechanisms)
+            # TODO(kgiusti): server if accepting inbound connections
+            pn_sasl.client()
+        self.connection.open()
+
+    def reset(self, name=None):
+        self._container.remove_connection(self.name)
+        if name:
+            self.name = name
+        c = self._container.create_connection(self.name, self._handler,
+                                              self._properties)
+        c.user_context = self
+        self.connection = c
+
+
+class Schedule(object):
+    def __init__(self):
+        self._entries = []
+
+    def schedule(self, request, delay):
+        entry = (time.time() + delay, request)
+        heapq.heappush(self._entries, entry)
+
+    def timeout(self, t):
+        due = self.next()
+        if not due:
+            return t
+        now = time.time()
+        if due < now:
+            return 0
+        else:
+            return min(due - now, t) if t else due - now
+
+    def next(self):
+        return self._entries[0][0] if len(self._entries) else None
+
+    def process(self):
+        n = self.next()
+        while n and n < time.time():
+            heapq.heappop(self._entries)[1]()
+            n = self.next()
+
+
+class Requests(object):
+    def __init__(self):
+        self._requests = moves.queue.Queue()
+        self._wakeup_pipe = os.pipe()
+
+    def wakeup(self, request=None):
+        if request:
+            self._requests.put(request)
+        os.write(self._wakeup_pipe[1], "!")
+
+    def fileno(self):
+        return self._wakeup_pipe[0]
+
+    def read(self):
+        os.read(self._wakeup_pipe[0], 512)
+        while not self._requests.empty():
+            self._requests.get()()
+
+
+class Thread(threading.Thread):
+
+    def __init__(self, container_name=None):
+        threading.Thread.__init__(self)
+
+        # handle requests from other threads
+        self._requests = Requests()
+        # handle delayed tasks (only used on this thread for now)
+        self._schedule = Schedule()
+
+        # Configure a container
+        if container_name:
+            self._container = engine.Container(container_name)
+        else:
+            self._container = engine.Container(uuid.uuid4().hex)
+
+        self.name = "Thread for Proton container: %s" % self._container.name
+        self._shutdown = False
+        self.daemon = True
+        self.start()
+
+    def wakeup(self, request=None):
+        """Wake up the processing thread."""
+        self._requests.wakeup(request)
+
+    def schedule(self, request, delay):
+        self._schedule.schedule(request, delay)
+
+    def destroy(self):
+        """Stop the processing thread, releasing all resources.
+        """
+        LOG.debug("Stopping Proton container %s" % self._container.name)
+        self.wakeup(lambda: self._do_shutdown())
+        self.join()
+
+    def _do_shutdown(self):
+        self._shutdown = True
+
+    def connect(self, hostname, port, handler, properties={}, name=None,
+                sasl_mechanisms="ANONYMOUS"):
+        """Get a _SocketConnection to a peer represented by url."""
+        key = name or "%s:%i" % (hostname, port)
+        # return pre-existing
+        conn = self._container.get_connection(key)
+        if conn:
+            return conn.user_context
+
+        # create a new connection - this will be stored in the
+        # container, using the specified name as the lookup key, or if
+        # no name was provided, the host:port combination
+        sc = _SocketConnection(key, self._container,
+                               properties, handler=handler)
+        sc.connect(hostname, port, sasl_mechanisms)
+        return sc
+
+    def run(self):
+        """Run the proton event/timer loop."""
+        LOG.debug("Starting Proton thread, container=%s",
+                  self._container.name)
+
+        while not self._shutdown:
+            readers, writers, timers = self._container.need_processing()
+
+            readfds = [c.user_context for c in readers]
+            # additionally, always check for readability of pipe we
+            # are using to wakeup processing thread by other threads
+            readfds.append(self._requests)
+            writefds = [c.user_context for c in writers]
+
+            timeout = None
+            if timers:
+                deadline = timers[0].next_tick  # 0 == next expiring timer
+                now = time.time()
+                timeout = 0 if deadline <= now else deadline - now
+
+            timeout = self._schedule.timeout(timeout)
+
+            results = select.select(readfds, writefds, [], timeout)
+            readable, writable, ignore = results
+
+            for r in readable:
+                r.read()
+
+            self._schedule.process()
+            for t in timers:
+                if t.next_tick > time.time():
+                    break
+                t.process(time.time())
+
+            for w in writable:
+                w.write()
+
+        LOG.debug("Stopping Proton thread, container=%s",
+                  self._container.name)
+        # abort any requests. Setting the lists to None here prevents further
+        # requests from being created
